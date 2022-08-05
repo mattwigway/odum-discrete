40589506b981f692af29371182a53b053d833e5d
Matt Bhagat-Conway <matt@indicatrix.org>
2022-08-05 12:52:24 -0400

    update to use Apollo mode choice dataset

--- mode-choice-mnl.R
+++ mode-choice-mnl.R
@@ -1,13 +1,13 @@
 library(apollo)
 library(tidyverse)
 
-database = read_csv("data/modechoice.csv")
+database = read_csv("data/modechoice_apollo.csv")
 
 apollo_initialise()
 
 apollo_control = list(
-    modelName = "Australia_Mode_Choice",
-    indivID="individual"
+    modelName = "Apollo_Mode_Choice",
+    indivID="ID"
 )
 
 head(database)
@@ -15,18 +15,16 @@ head(database)
 apollo_beta = c(
     asc_air = 0,
     asc_bus = 0,
-    asc_train = 0,
+    asc_rail = 0,
     # leaving out an asc_car as base category
     b_cost = 0,
+    b_access_time = 0,
     b_in_vehicle_time = 0,
     # income and party_size are individual level variable, so
     # must be different for different alternatives
-    b_hhincome_air = 0,
-    b_hhincome_bus = 0,
-    b_hhincome_train = 0,
-    b_party_size_air = 0,
-    b_party_size_bus = 0,
-    b_party_size_train = 0
+    b_income_air = 0,
+    b_income_bus = 0,
+    b_income_rail = 0
 )
 
 apollo_fixed = c()
@@ -45,35 +43,38 @@ apollo_probabilities = function(apollo_beta, apollo_inputs,
     # We can include cost and in-vehicle time here because they vary over
     # alternatives
     V[["car"]] = b_cost * cost_car +
-        b_in_vehicle_time * in_vehicle_time_car
-    # Exercise: why can't we include wait time in utility of car?
+        b_in_vehicle_time * time_car
 
     V[["air"]] = asc_air +
         b_cost * cost_air +
-        b_in_vehicle_time * in_vehicle_time_air +
-        b_hhincome_air * hhincome_thousands +
-        b_party_size_air * party_size
+        b_in_vehicle_time * time_air +
+        b_access_time * access_air +
+        b_income_air * income
 
-    V[["train"]] = asc_train +
-        b_cost * cost_train +
-        b_in_vehicle_time * in_vehicle_time_train +
-        b_hhincome_train * hhincome_thousands +
-        b_party_size_train * party_size
+    V[["rail"]] = asc_rail +
+        b_cost * cost_rail +
+        b_in_vehicle_time * time_rail +
+        b_access_time * access_rail +
+        b_income_rail * income
 
     V[["bus"]] = asc_bus +
         b_cost * cost_bus +
-        b_in_vehicle_time * in_vehicle_time_bus +
-        b_hhincome_bus * hhincome_thousands +
-        b_party_size_bus * party_size
+        b_in_vehicle_time * time_bus +
+        b_access_time * access_bus +
+        b_income_bus * income
 
     mnl_settings = list(
-        alternatives = c(car="car", air="air", train="train", bus="bus"),
-        avail = list(car=T, air=T, train=T, bus=T),
-        choiceVar = chosen,
+        alternatives = c(car=1, air=3, rail=4, bus=2),
+        avail = list(car=av_car, air=av_air, rail=av_rail, bus=av_bus),
+        choiceVar = choice,
         utilities = V
     )
 
     P[["model"]] = apollo_mnl(mnl_settings, functionality)
+
+    # For panel data, this multiplies observations for single individuals
+    # together - no effect in multinomial logit but still required.
+    P = apollo_panelProd(P, apollo_inputs, functionality)
     P = apollo_prepareProb(P, apollo_inputs, functionality)
     return(P)
 }
@@ -84,4 +85,11 @@ model = apollo_estimate(apollo_beta, apollo_fixed,
 
 # Print results
 apollo_modelOutput(model)
+
+apollo_deltaMethod(model, deltaMethod_settings=list(
+    operation="ratio",
+    parName1="b_in_vehicle_time",
+    parName2="b_cost",
+    multPar1=60
+))
 
